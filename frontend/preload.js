24019f7ae9da5b0a8db0b8580c949ae3bcdfcee2
FernandoAFOliveira <ffontelesoliveira@mail.valenciacollege.edu>
2025-05-06 21:46:53 -0400

    installed continue and setup ollama 3 8b

--- frontend/preload.js
+++
@@ -1,9 +0,0 @@
-// frontend/preload.js
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electron', {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  receive: (channel, fn) => {
-    ipcRenderer.on(channel, (_, ...args) => fn(...args))
-  }
-})
